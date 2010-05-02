f6513f35a5c06e8706de502f50cfd4f059c10bb5
Michael Granados <michaelgranados@gmail.com>
2010-05-02 03:31:45 -0300

    Tela de login

---
+++ template/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <title>Tuittai - Login</title>
+</head>
+<body>
+
+    <form action="<?php echo BASE_URL ?>" method="post">
+        <label>
+            <span>Login</span>
+            <input type="text" name="login" value="" />
+        </label>
+        <label>
+            <span>Senha</span>
+            <input type="password" name="senha" value="" />
+        </label>
+        <input type="submit" class="submit" value="Entrar" name="" />
+    </form>
+
+</body>
+</html>
